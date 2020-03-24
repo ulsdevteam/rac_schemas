0143cb50a2515ab005d97151e377f74b0011cea6
Hillel Arnold <helrond@hotmail.com>
2020-03-24 19:56:42 -0400

    moving location of schemas dir inside package

--- rac_schemas/__init__.py
+++ rac_schemas/__init__.py
@@ -1,9 +1,7 @@
 import json
-from os.path import abspath, dirname, join
+from os.path import join
 
 from jsonschema import Draft7Validator, RefResolver
-
-base_path = dirname(dirname(abspath(__file__)))
 
 
 def handle_schema_filename(filename):
@@ -22,9 +20,9 @@ def is_valid(data, schema_name):
             "Data to be validated must be a dict, got {} instead".format(
                 type(data)))
     filename = handle_schema_filename(schema_name)
-    with open(join(base_path, "schemas", "base.json"), "r") as bf:
+    with open(join("schemas", "base.json"), "r") as bf:
         base_schema = json.load(bf)
-        with open(join(base_path, "schemas", filename), "r") as sf:
+        with open(join("schemas", filename), "r") as sf:
             object_schema = json.load(sf)
             resolver = RefResolver.from_schema(base_schema)
             validator = Draft7Validator(

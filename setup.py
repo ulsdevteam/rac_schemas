abf22b1af59a5a493829a79fa4d973a4fa652798
Hillel Arnold <helrond@hotmail.com>
2020-03-24 17:15:59 -0400

    move lists of objects to their own key, fixes #3

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='rac_schemas',
-      version='0.1',
+      version='0.2',
       description='RAC JSON Schemas and validators',
       url='http://github.com/RockefellerArchiveCenter/rac_schemas',
       author='Rockefeller Archive Center',
       author_email='archive@rockarch.org',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       install_requires=['jsonschema'],
       license='MIT',
       packages=['rac_schemas'],
       test_suite='nose.collector',
       tests_require=['nose', 'jsonschema'],
-      zip_safe=False)
+      zip_safe=False,
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6',)

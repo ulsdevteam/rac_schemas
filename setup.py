3cb3ce1f9a5386123e6c973365d559a662782fc0
Hillel Arnold <helrond@hotmail.com>
2020-08-13 15:23:34 -0400

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='rac_schemas',
-      version='0.13',
+      version='0.14',
       description='RAC JSON Schemas and validators',
       url='http://github.com/RockefellerArchiveCenter/rac_schemas',
       author='Rockefeller Archive Center',

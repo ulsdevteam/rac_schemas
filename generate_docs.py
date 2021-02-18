67680dd965b263c8fdd78ee7b3a8268432066371
Patrick <pgalligan@rockarch.org>
2021-02-18 10:55:26 -0500

    Fix linting
    Fix linting and typo.

--- generate_docs.py
+++ generate_docs.py
@@ -3,8 +3,10 @@ import os
 
 from json_schema_for_humans.generate import generate_from_filename
 
+
 class DocumentGenerator:
     '''Generates HTML documentation based on JSON schema files.'''
+
     def run(self, json_directory, html_directory):
         '''Iterates over a directory and generates an HTML schema file from any file ending in .json
 
@@ -14,11 +16,20 @@ class DocumentGenerator:
         for file in os.listdir(json_directory):
             if file.endswith(".json"):
                 html_file = "{}.html".format(file.split(".")[0])
-                generate_from_filename(os.path.join(json_directory, file), os.path.join(html_directory, html_file))
+                generate_from_filename(
+                    os.path.join(
+                        json_directory, file), os.path.join(
+                        html_directory, html_file))
 
-parser = argparse.ArgumentParser(description="Creates HTML files based on JSON schemas.")
-parser.add_argument("json_directory", help="Directory containing JSON Schema files.")
-parser.add_argument("html_directory", help="Directory containing JSON Schema files.")
+
+parser = argparse.ArgumentParser(
+    description="Creates HTML files based on JSON schemas.")
+parser.add_argument(
+    "json_directory",
+    help="Directory containing JSON Schema files.")
+parser.add_argument(
+    "html_directory",
+    help="Directory to write HTML files to.")
 args = parser.parse_args()
 
 DocumentGenerator().run(args.json_directory, args.html_directory)

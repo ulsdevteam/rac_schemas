70b93be9d5e9c5d841f718ac24fa5f481e190293
Patrick <pgalligan@rockarch.org>
2021-02-22 11:00:13 -0500

    Update generate_docs.py
    Updates docstrings and add list comprehension

--- generate_docs.py
+++ generate_docs.py
@@ -5,21 +5,20 @@ from json_schema_for_humans.generate import generate_from_filename
 
 
 class DocumentGenerator:
-    '''Generates HTML documentation based on JSON schema files.'''
+    """Generates HTML documentation based on JSON schema files."""
 
     def run(self, json_directory, html_directory):
-        '''Iterates over a directory and generates an HTML schema file from any file ending in .json
+        """Iterates over a directory and generates an HTML schema file from any file ending in .json
 
         args:
             directory(str): a string representation of the full filepath of the directory containing schema files
-        '''
-        for file in os.listdir(json_directory):
-            if file.endswith(".json"):
-                html_file = "{}.html".format(file.split(".")[0])
-                generate_from_filename(
-                    os.path.join(
-                        json_directory, file), os.path.join(
-                        html_directory, html_file))
+        """
+        filename = [file for file in os.listdir(json_directory) if if file.endswith(".json")]
+        html_file = "{}.html".format(filename.split(".")[0])
+        generate_from_filename(
+            os.path.join(
+                json_directory, filename), os.path.join(
+                html_directory, html_file))
 
 
 parser = argparse.ArgumentParser(

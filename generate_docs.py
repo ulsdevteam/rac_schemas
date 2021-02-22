795ff4c602f58b0834f5075ee7aecca63246b911
Hillel Arnold <helrond@hotmail.com>
2021-02-22 13:27:26 -0500

    refactor as functional, add module-level docstring

--- generate_docs.py
+++ generate_docs.py
@@ -1,26 +1,29 @@
+"""Generates HTML documentation from JSONSchema files.
+
+Assumes JSONSchema files are located in a directory named `/rac_schemas/schemas`.
+HTML documentation will be created in a directory called `/docs`.
+
+This script requires the Python package [json_schema_for_humans](https://pypi.org/project/json-schema-for-humans/).
+"""
+
 import os
 from pathlib import Path
 
 from json_schema_for_humans.generate import generate_from_filename
 
 
-class DocumentGenerator:
-    """Generates HTML documentation based on JSON schema files."""
-
-    def run(self):
-        """Iterates over a directory and generates an HTML schema file from any file ending in .json"""
-        project_directory = Path(__file__).parent.absolute()
-        json_directory = os.path.join(
-            project_directory, "rac_schemas", "schemas")
-        html_directory = os.path.join(project_directory, "docs")
-        json_files = [file for file in os.listdir(
-            json_directory) if file.endswith(".json")]
-        for schema in json_files:
-            html_file = "{}.html".format(schema.split(".")[0])
-            generate_from_filename(
-                os.path.join(
-                    json_directory, schema), os.path.join(
-                    html_directory, html_file))
+def main():
+    project_directory = Path(__file__).parent.absolute()
+    json_directory = os.path.join(
+        project_directory, "rac_schemas", "schemas")
+    html_directory = os.path.join(project_directory, "docs")
+    for schema in [f for f in os.listdir(json_directory) if f.endswith(".json")]:
+        html_file = "{}.html".format(schema.split(".")[0])
+        generate_from_filename(
+            os.path.join(
+                json_directory, schema), os.path.join(
+                html_directory, html_file))
 
 
-DocumentGenerator().run()
+if __name__ == "__main__":
+    main()
